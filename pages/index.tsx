e4fbcff354f442d9698bf66401eee417e0b4fdff
Aakash Jha <aakash.jha1103@gmail.com>
2023-03-15 00:34:08 +0530

    SEO improvement

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,27 @@ import { NextPage } from "next";
 import styles from "@/styles/homepage.module.css";
 import { Button } from "@/components";
 import { PageContainer } from "@/components/Container/PageContainer";
+import Head from "next/head";
 
 const Homepage: NextPage = () => {
 	return (
-		<div className={`${styles.homepage}`}>
-			<section className={`${styles.hero}`}>
-				<PageContainer>
-					<div className={`${styles.hero_col_left}`}>
-						<h1>Access a library of free design resources</h1>
-						<h2>The best resources and books from around the web, collected and curated for your reading.</h2>
-						<Button label="Get Access Now" />
-					</div>
-					<div className={`${styles.hero_col_right}`}>s</div>
-				</PageContainer>
-			</section>
-		</div>
+		<>
+			<Head>
+				<title>MemberBase - Home</title>
+			</Head>
+			<div className={`${styles.homepage}`}>
+				<section className={`${styles.hero}`}>
+					<PageContainer>
+						<div className={`${styles.hero_col_left}`}>
+							<h1>Access a library of free design resources</h1>
+							<h2>The best resources and books from around the web, collected and curated for your reading.</h2>
+							<Button label="Get Access Now" />
+						</div>
+						<div className={`${styles.hero_col_right}`}>s</div>
+					</PageContainer>
+				</section>
+			</div>
+		</>
 	);
 };
 
