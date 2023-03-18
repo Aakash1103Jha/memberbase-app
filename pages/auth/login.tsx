862fd141903bffd3c85b9dcba08535cc19556ee5
Aakash Jha <aakash.jha1103@gmail.com>
2023-03-18 12:34:53 +0530

    Complete Login and Register flow

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,14 +1,45 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
+import { signIn } from "next-auth/react";
 
 import styles from "@/styles/login.module.css";
 
-import LOGO from "@/assets/svg/memberbase.svg";
 import { PageContainer } from "@/components/Container";
 import { Button, Textbox } from "@/components";
-import Link from "next/link";
 
 const Login: NextPage = () => {
+	const router = useRouter();
+	const [error, setError] = useState<string | undefined>(undefined);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const onEmailChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+		setError("");
+		return setEmail(e.target.value);
+	};
+	const onPasswordChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+		setError("");
+		return setPassword(e.target.value);
+	};
+	const onLogin: FormEventHandler<HTMLFormElement> = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await signIn("credentials", {
+				email,
+				password,
+				redirect: false,
+				callbackUrl: "/",
+			});
+			if (res?.error) return setError(res.error);
+			return router.replace("/");
+		} catch (error) {
+			const { message } = error as Error;
+			return setError(message);
+		}
+	};
 	return (
 		<>
 			<Head>
@@ -16,14 +47,29 @@ const Login: NextPage = () => {
 			</Head>
 			<div className={`${styles.login_page}`}>
 				<PageContainer>
-					<form className={`${styles.login_form}`}>
+					<form className={`${styles.login_form}`} onSubmit={onLogin}>
 						<div className={`${styles.login_form_header}`}>
 							<h1>Log in</h1>
 							<h3>Fill in your login details below</h3>
 						</div>
 						<div className={`${styles.login_form_fields}`}>
-							<Textbox label="Email" placeholder="Your email" required type="email" />
-							<Textbox label="Password" placeholder="Your Password" required type="password" />
+							<Textbox
+								label="Email"
+								placeholder="Your email"
+								required
+								type="email"
+								value={email}
+								onChange={onEmailChange}
+							/>
+							<Textbox
+								label="Password"
+								placeholder="Your Password"
+								required
+								type="password"
+								value={password}
+								onChange={onPasswordChange}
+							/>
+							{error ? <p className="error">{error}</p> : null}
 							<Button label="Log in" />
 						</div>
 						<p className={`${styles.login_form_cta}`}>
