c6b46f98c77f55eb96c831a94a289f355657dbed
Aakash Jha <aakash.jha1103@gmail.com>
2023-03-22 22:52:41 +0530

    Added TnC page and context

--- pages/info/tnc.tsx
+++ pages/info/tnc.tsx
@@ -8,10 +8,24 @@ import { PageContainer } from "@/components/Container";
 import { sendAnalytics } from "@/lib/sendAnalytics";
 import { useEffectOnce } from "@/hooks/useEffectOnce";
 
+import CONFIG from "@/config/tnc.config.json";
+
 const TnC: NextPage = () => {
 	useEffectOnce(() => {
 		sendAnalytics();
 	});
+	const ListItem = (data: { title: string; text: string[] }) => {
+		return (
+			<div className={`${styles.tnc_page_list_item}`}>
+				<h3>{data.title}</h3>
+				<ul>
+					{data.text.map((node) => (
+						<li key={node}>{node}</li>
+					))}
+				</ul>
+			</div>
+		);
+	};
 	return (
 		<>
 			<Head>
@@ -20,6 +34,16 @@ const TnC: NextPage = () => {
 			<PageContainer>
 				<div className={`${styles.tnc_page}`}>
 					<h1>Terms & Conditions</h1>
+					<h2>
+						Welcome to our website! Please read the following terms and conditions carefully before using our site.
+					</h2>
+					<ol className={`${styles.tnc_page_list}`}>
+						{CONFIG.map((textNode) => (
+							<li key={textNode.title}>
+								<ListItem {...textNode} />
+							</li>
+						))}
+					</ol>
 				</div>
 			</PageContainer>
 		</>
