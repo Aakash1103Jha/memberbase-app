38be530fcc2bebf48b7039f10ae88928f88f44a1
Aakash Jha <aakash.jha1103@gmail.com>
2023-03-22 23:26:57 +0530

    Added Privacy policy page

--- pages/info/privacy.tsx
+++ pages/info/privacy.tsx
@@ -7,20 +7,42 @@ import { PageContainer } from "@/components/Container";
 import { sendAnalytics } from "@/lib/sendAnalytics";
 import { useEffectOnce } from "@/hooks/useEffectOnce";
 
+import CONFIG from "@/config/privacy.config.json";
+
 const Privacy: NextPage = () => {
 	useEffectOnce(() => {
 		sendAnalytics();
 	});
+	const ListItem = (data: { title: string; text: string[] }) => {
+		return (
+			<div className={`${styles.privacy_page_list_item}`}>
+				<h3>{data.title}</h3>
+				<ul>
+					{data.text.map((node) => (
+						<li key={node}>{node}</li>
+					))}
+				</ul>
+			</div>
+		);
+	};
 	return (
 		<>
 			<Head>
 				<title>MemberBase - Privacy</title>
 			</Head>
-			<div className={`${styles.privacy_page}`}>
-				<PageContainer>
-					<h1>Privacy Policy</h1>
-				</PageContainer>
-			</div>
+			<PageContainer>
+				<div className={`${styles.privacy_page}`}>
+					<h1>Privacy</h1>
+					<h2>Welcome to our website! Please read the following privacy policy carefully before using our site.</h2>
+					<ol className={`${styles.privacy_page_list}`}>
+						{CONFIG.map((textNode) => (
+							<li key={textNode.title}>
+								<ListItem {...textNode} />
+							</li>
+						))}
+					</ol>
+				</div>
+			</PageContainer>
 		</>
 	);
 };
